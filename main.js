c8278b123374745dd67199becb74f1894e60296f
erika2C <128616925+erika2C@users.noreply.github.com>
2024-06-11 12:36:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+{
+    enunciado: "pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+    {
+        
+    }
+]
